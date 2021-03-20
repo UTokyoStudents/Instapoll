190ae4425bac8f5645a33ef3f2c474da7437de59
metastable-void <git@yuka.h09.eu>
2021-03-20 19:46:39 +0900

    domain redirect

--- firebase/public/assets/main.mjs
+++ firebase/public/assets/main.mjs
@@ -1,1 +1,9 @@
+
+const CANONICAL_DOMAIN = 'ut-vote.web.app';
+
+if (location.hostname != CANONICAL_DOMAIN) {
+    location.hostname = CANONICAL_DOMAIN;
+    throw void 0;
+}
+
 console.log('loaded');
