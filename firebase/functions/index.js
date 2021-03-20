6039275a8feb90de3c00e204af7b6d4a1a969139
metastable-void <git@yuka.h09.eu>
2021-03-21 01:04:41 +0900

    vote count

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,6 +7,16 @@ admin.initializeApp();
 
 const ticketsCollection = admin.firestore().collection('tickets');
 
+const validateUuid = uuid => !!String(uuid).match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/);
+
+const formatUuid = buffer => [
+    buffer.slice(0, 4).toString('hex'),
+    buffer.slice(4, 6).toString('hex'),
+    buffer.slice(6, 8).toString('hex'),
+    buffer.slice(8, 10).toString('hex'),
+    buffer.slice(10, 16).toString('hex'),
+].join('-');
+
 exports.addTicket = functions.https.onRequest(async (req, res) => {
     try {
         const publicKey = Buffer.from(String(req.query.publicKey || ''), 'base64');
@@ -14,7 +24,7 @@ exports.addTicket = functions.https.onRequest(async (req, res) => {
             throw new TypeError('Invalid public key length');
         }
         const voteId = String(req.query.voteId || '').toLowerCase();
-        if (!voteId.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)) {
+        if (!validateUuid(voteId)) {
             throw new TypeError('Invalid UUID');
         }
         const base64PublicKey = publicKey.toString('base64');
@@ -48,6 +58,9 @@ exports.vote = functions.https.onRequest(async (req, res) => {
             throw new TypeError('Invalid signature length');
         }
         const data = Buffer.from(String(req.query.data || ''), 'base64');
+        if (data.length != 16) {
+            throw new TypeError('Invalid data');
+        }
         const ticketDoc = await ticketsCollection.doc(base64PublicKey).get();
         if (!ticketDoc.exists) {
             throw new TypeError('Ticket not found');
@@ -61,11 +74,39 @@ exports.vote = functions.https.onRequest(async (req, res) => {
         }
         await ticketsCollection.doc(base64PublicKey).set({
             voted: true,
-            vote_data: data.toString('base64'),
+            vote_data: formatUuid(data),
             signature: signature.toString('base64'),
         }, {merge: true});
         res.status(200).json({
             error: null,
+        });
+    } catch (e) {
+        res.status(400).json({
+            error: String(e),
+        });
+    }
+});
+
+exports.count = functions.https.onRequest(async (req, res) => {
+    try {
+        const voteId = String(req.query.voteId || '');
+        if (!validateUuid(voteId)) {
+            throw new TypeError('Invalid UUID');
+        }
+        const ticketsSnapshot = await ticketsCollection.where('vote_id', '==', voteId).get();
+        const results = Object.create(null);
+        const votesCount = 0;
+        for (const ticket of ticketsSnapshot) {
+            if (!ticket.voted) {
+                continue;
+            }
+            votesCount++;
+            results[ticket.vote_data] = 1 + 0 | results[ticket.vote_data];
+        }
+        res.status(200).json({
+            error: null,
+            votesCount,
+            results,
         });
     } catch (e) {
         res.status(400).json({
